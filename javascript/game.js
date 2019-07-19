330dbb370a3aa9f5bfdad0f470c93903a89cb6d2
Yegor B <biryuky1@tcnj.edu>
2019-07-18 20:40:19 -0400

    finisehd crystals collector, no formatting

---
+++ javascript/game.js
@@ -0,0 +1,64 @@
+function RandomIntFromInterval(min, max) // min and max included
+{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var computerNum
+var gem1
+var gem2
+var gem3
+var gem4
+
+var totalScore
+
+var wins = 0
+var losses = 0
+
+ResetVars()
+
+
+$(".gem").click(function () {
+    var gemVal = parseInt($(this).attr("value"))
+    totalScore = Number(totalScore + gemVal)
+
+    $("#totalScore").text(totalScore)
+
+    if (totalScore == computerNum) {
+        YouWin()
+    } else if (totalScore > computerNum) {
+        YouLose()
+    }
+})
+
+
+function YouWin() {
+    wins++
+    $("#resultMsg").text("You won!!")
+    $("#wins").text("Wins: " + wins)
+    ResetVars()
+}
+
+function YouLose() {
+    losses++
+    $("#resultMsg").text("You lost!!")
+    $("#losses").text("Losses: " + wins)
+    ResetVars()
+}
+
+function ResetVars() {
+    computerNum = RandomIntFromInterval(19, 120)
+    gem1 = RandomIntFromInterval(1, 12)
+    gem2 = RandomIntFromInterval(1, 12)
+    gem3 = RandomIntFromInterval(1, 12)
+    gem4 = RandomIntFromInterval(1, 12)
+
+    totalScore = 0
+
+    $(".gem1").attr("value", gem1)
+    $(".gem2").attr("value", gem2)
+    $(".gem3").attr("value", gem3)
+    $(".gem4").attr("value", gem4)
+
+    $("#randonNum").text(computerNum)
+    $("#totalScore").text(0)
+}
